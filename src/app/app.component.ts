e909e040648555388cc661971ec25e83c4b7a925
nguetcheu <dnguetcheu@gmail.com>
2023-04-06 20:38:03 +0200

    usage de ma méthode ngOnInit pour afficher un console.Table()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  pokemons = ['Bulbizarre', 'Salamèche', 'Carapuce'];
+export class AppComponent implements OnInit{
+  pokemonList = ['Bulbizarre', 'Salamèche', 'Carapuce'];
+
+  ngOnInit() {
+    console.table(this.pokemonList);
+  }
+
 }
