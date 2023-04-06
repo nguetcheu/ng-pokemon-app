fa6f0485f3eb17007b04126d32ac6a8f26f7e597
nguetcheu <dnguetcheu@gmail.com>
2023-04-06 20:49:09 +0200

    ajout fichier de la classe Pokemon et du fichier Mock contenat une liste de pokemon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   pokemonList = ['Bulbizarre', 'Salamèche', 'Carapuce'];
 
   ngOnInit() {
     console.table(this.pokemonList);
+    this.selectPokemon('Pikachu');
   }
 
+  selectPokemon(pokemonName: String) {
+    console.log(`Vous avez cliqué sur le pokémon ${pokemonName}`);
+  }
 }
