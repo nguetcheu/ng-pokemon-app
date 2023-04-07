651c91950fa347e50d6ded6589da8a275717dfd2
nguetcheu <dnguetcheu@gmail.com>
2023-04-07 14:55:51 +0200

    meilleure gestion interaction avec utilisateur

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   pokemonList: Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon | undefined;
 
   ngOnInit() {
     console.table(this.pokemonList);
   }
 
-  selectPokemon(event: MouseEvent) {
-    const index: number = +(event.target as HTMLInputElement).value;
-    console.log(`Vous avez cliqué sur le pokémon ${this.pokemonList[index].name}`);
+  selectPokemon(pokemonId: String) {
+    const pokemon: Pokemon | undefined = this.pokemonList.find(
+      (pokemon) => pokemon.id == +pokemonId
+    );
+    if (pokemon) {
+      console.log(`Vous avez demandé un pokémon le pokémon ${pokemon.name}`);
+      this.pokemonSelected = pokemon;
+    } else {
+      console.log(`Vous avez demandé un pokémon qui n'existe pas`);
+      this.pokemonSelected = pokemon;
+    }
   }
 }
