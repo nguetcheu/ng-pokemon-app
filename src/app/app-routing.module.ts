13c08a734bf7d08244ab3bc6880f0503f7a1cdfc
nguetcheu <dnguetcheu@gmail.com>
2023-04-08 14:50:00 +0200

    fix erreur les routes n'ont pas besoin de / devant leur path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,9 @@ import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
 import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
 
 const routes: Routes = [
-  { path: '/pokemons', component: ListPokemonComponent },
-  { path: '/pokemons/:id', component: DetailPokemonComponent },
-  { path: '', redirectTo: '/pokemons', pathMatch: 'full' },
+  { path: 'pokemons', component: ListPokemonComponent },
+  { path: 'pokemon/:id', component: DetailPokemonComponent },
+  { path: '', redirectTo: 'pokemons', pathMatch: 'full' },
 ];
 
 @NgModule({
