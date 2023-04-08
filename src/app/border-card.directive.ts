821e237dd052e32f5de632e6476343572148dc01
nguetcheu <dnguetcheu@gmail.com>
2023-04-08 11:51:03 +0200

    code de la directive

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -1,10 +1,19 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
-  selector: '[appBorderCard]'
+  selector: '[pkmnBorderCard]',
 })
 export class BorderCardDirective {
+  constructor(private el: ElementRef) {
+    this.setHeight(180);
+    this.setBorder('#f5f5f5');
+  }
 
-  constructor() { }
+  setHeight(height: number) {
+    this.el.nativeElement.style.height = `${height}px`;
+  }
 
+  setBorder(color: string) {
+    this.el.nativeElement.syle.border = `solid 4px ${color}`;
+  }
 }
