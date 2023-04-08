e5cb8291aadd166d7a8abc71582ccc428712dd44
nguetcheu <dnguetcheu@gmail.com>
2023-04-08 11:57:10 +0200

    ajout de @HostListener dans le but de reagir au event mouseenter et mouseleave de l'utilisateur avec sa souris

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[pkmnBorderCard]',
@@ -9,11 +9,20 @@ export class BorderCardDirective {
     this.setBorder('#f5f5f5');
   }
 
+  @HostListener('mouseenter') onMouseEnter() {
+    this.setBorder('#009688');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.setBorder('#f5f5f5');
+  }
+
   setHeight(height: number) {
     this.el.nativeElement.style.height = `${height}px`;
   }
 
   setBorder(color: string) {
-    this.el.nativeElement.syle.border = `solid 4px ${color}`;
+    let border = 'solid 4px' + color;
+    this.el.nativeElement.syle.border = border;
   }
 }
