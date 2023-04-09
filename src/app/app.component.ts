55c563229fb32f9604e2779a96781f51c8e231d7
nguetcheu <dnguetcheu@gmail.com>
2023-04-09 12:33:30 +0200

    nettoyage de app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,8 @@
-import { POKEMONS } from './mock-pokemon-list';
-import { Pokemon } from './pokemon';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  pokemonList: Pokemon[] = POKEMONS;
-  pokemonSelected: Pokemon | undefined;
-
-  ngOnInit() {
-    console.table(this.pokemonList);
-  }
-
-  selectPokemon(pokemonId: String) {
-    const pokemon: Pokemon | undefined = this.pokemonList.find(
-      (pokemon) => pokemon.id == +pokemonId
-    );
-    if (pokemon) {
-      console.log(`Vous avez demandé un pokémon le pokémon ${pokemon.name}`);
-      this.pokemonSelected = pokemon;
-    } else {
-      console.log(`Vous avez demandé un pokémon qui n'existe pas`);
-      this.pokemonSelected = undefined;
-    }
-  }
-}
+export class AppComponent {}
