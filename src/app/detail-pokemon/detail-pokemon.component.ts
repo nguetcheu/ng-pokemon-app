3f5f1101c79c2f556726480e51b042d49eb9ba20
nguetcheu <dnguetcheu@gmail.com>
2023-04-09 13:06:06 +0200

    usage de activatedRoute pour récuperer la propriéte id

--- src/app/detail-pokemon/detail-pokemon.component.ts
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { POKEMONS } from '../mock-pokemon-list';
+import { Pokemon } from '../pokemon';
 
 @Component({
   selector: 'app-detail-pokemon',
   templateUrl: './detail-pokemon.component.html',
-  styleUrls: ['./detail-pokemon.component.css']
+  styleUrls: ['./detail-pokemon.component.css'],
 })
 export class DetailPokemonComponent implements OnInit {
+  pokemonList: Pokemon[];
+  pokemon: Pokemon | undefined;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.pokemonList = POKEMONS;
+    const pokemonId: string | null = this.route.snapshot.paramMap.get('id');
+
+    if (pokemonId) {
+      this.pokemon = this.pokemonList.find(
+        (pokemon) => pokemon.id == +pokemonId
+      );
+    } 
   }
-
 }
