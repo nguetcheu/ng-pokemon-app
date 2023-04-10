d2d6c52c7a204809d1590d3abe192fb6e9371515
nguetcheu <dnguetcheu@gmail.com>
2023-04-10 21:56:03 +0200

    création des méthodes de manipluation des pokémons dans le service pokemon.service

--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Pokemon } from './pokemon';
+import { POKEMONS } from './mock-pokemon-list';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PokemonService {
+  getPokemonList(): Pokemon[] {
+    return POKEMONS;
+  }
 
-  constructor() { }
+  getPokemonById(pokemonId: number): Pokemon | undefined {
+    return POKEMONS.find((pokemon) => pokemon.id == pokemonId);
+  }
+
+  getPokemonTypeList(): string[] {
+    return [
+      'Plante',
+      'Feu',
+      'Eau',
+      'Insecte',
+      'Normal',
+      'Electrik',
+      'Poisson',
+      'Fée',
+      'Vol',
+      'Combat',
+      'Psy',
+    ];
+  }
 }
