59a535d4672194b036675d772c55605589be2a22
nguetcheu <dnguetcheu@gmail.com>
2023-04-11 23:03:12 +0200

    gestion de affichage du  formulaire pokémon

--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -8,8 +8,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { PokemonService } from './pokemon.service';
 import { FormsModule } from '@angular/forms';
 import { PokemonFormComponent } from './pokemon-form/pokemon-form.component';
+import { EditPokemonComponent } from './edit-pokemon/edit-pokemon.component';
 
 const pokemonRoute: Routes = [
+  { path: 'edit/pokemon/:id', component: EditPokemonComponent },
   { path: 'pokemons', component: ListPokemonComponent },
   { path: 'pokemon/:id', component: DetailPokemonComponent },
 ];
@@ -21,6 +23,7 @@ const pokemonRoute: Routes = [
     BorderCardDirective,
     PokemonTypeColorPipe,
     PokemonFormComponent,
+    EditPokemonComponent,
   ],
   imports: [CommonModule, FormsModule, RouterModule.forChild(pokemonRoute)],
   providers: [PokemonService],
