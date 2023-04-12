0be93009cd85af7cbeeb89e51196d2406f4d6296
nguetcheu <dnguetcheu@gmail.com>
2023-04-12 21:37:09 +0200

    ecriture des méthodes permettant de manipuler api

--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -1,15 +1,36 @@
 import { Injectable } from '@angular/core';
 import { Pokemon } from './pokemon';
-import { POKEMONS } from './mock-pokemon-list';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+
+import { catchError, tap } from 'rxjs/operators';
+import { error } from 'console';
 
 @Injectable()
 export class PokemonService {
-  getPokemonList(): Pokemon[] {
-    return POKEMONS;
+  constructor(private http: HttpClient) {}
+
+  getPokemonList(): Observable<Pokemon[]> {
+    return this.http.get<Pokemon[]>('api/pokemons').pipe(
+      tap((response) => this.log(response)),
+      catchError((error) => this.handleError(error, []))
+    );
   }
 
-  getPokemonById(pokemonId: number): Pokemon | undefined {
-    return POKEMONS.find((pokemon) => pokemon.id == pokemonId);
+  getPokemonById(pokemonId: number): Observable<Pokemon | undefined> {
+    return this.http.get<Pokemon>(`api/pokemons/${pokemonId}`).pipe(
+      tap((response) => this.log(response)),
+      catchError((error) => this.handleError(error, undefined))
+    );
+  }
+
+  private log(response: Pokemon[] | Pokemon | undefined) {
+    console.table(response);
+  }
+
+  private handleError(Error: Error, errorValue: any) {
+    console.error(error);
+    return of(errorValue);
   }
 
   getPokemonTypeList(): string[] {
